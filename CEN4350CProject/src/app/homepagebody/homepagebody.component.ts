0f4167fa5d4edd1b2999fb47e2d66925c228283a
rustly <43079937+Rustly@users.noreply.github.com>
2024-01-24 18:44:09 -0600

    initial commit and homepage components

---
+++ CEN4350CProject/src/app/homepagebody/homepagebody.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homepagebody',
+  templateUrl: './homepagebody.component.html',
+  styleUrl: './homepagebody.component.css'
+})
+export class HomepagebodyComponent {
+
+}
